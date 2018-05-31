1adac7e9ceae562276aa33254c6453ab6d29fc13
Annushka1 <robin1bobin@gmail.com>
2018-05-31 23:21:54 +0300

    карусели

--- src/js/script.js
+++ src/js/script.js
@@ -5,5 +5,80 @@ $('.gamburger').on('click', function(){
 
 $('.main-nav__item').on('click', function(){
   $('.main-nav').slideToggle();
-})
+});
+
+$('.responsive').slick({
+  dots: false,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  infinite: true,
+  speed: 300,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 320,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
+
+});
+$('.responsive2').slick({
+  dots: false,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  infinite: true,
+  speed: 300,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 320,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
+  
+});
 });
