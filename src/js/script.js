126698f03bb151d9d1bdb92ade2ebdf75198c423
Annushka1 <robin1bobin@gmail.com>
2018-06-03 04:41:57 +0300

    деск, решила проблему стрелок в галерее, при фиксированом контейнере

--- src/js/script.js
+++ src/js/script.js
@@ -15,7 +15,7 @@ $('.responsive').slick({
   speed: 300,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1920,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 3,
@@ -24,14 +24,14 @@ $('.responsive').slick({
       }
     },
     {
-      breakpoint: 800,
+      breakpoint: 1024,
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 3,
         slidesToScroll: 1
       }
     },
     {
-      breakpoint: 320,
+      breakpoint: 768,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
